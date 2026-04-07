f167543f4e35a09b52d6b21ab71618bc51b91efd
Mette Munne <mette.ruusu@gmail.com>
2026-04-07 21:08:25 +0300

    refactoring

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,20 +1,5 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.total === 0) {
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
-  }
-  return (
-    <div>
-      <Statistics good = {props.good} neutral = {props.neutral} bad = {props.bad} total = {props.total} average = {props.average} positive = {props.positive} />
-    </div>
-  )
-}
-
 const Button = (props) => (
   <button onClick={props.onClick}>
     {props.text}
@@ -22,14 +7,6 @@ const Button = (props) => (
 )
 
 const StatisticLine = (props) => {
-  if (props.text === "positive") {
-    return (
-      <tr>
-        <td>{props.text}</td>
-        <td>{props.value} %</td>
-      </tr>
-    )
-  }
   return (
     <tr>
       <td>{props.text}</td>
@@ -39,7 +16,17 @@ const StatisticLine = (props) => {
 }
 
 const Statistics = (props) => {
-  console.log(props);
+  const total = props.good + props.neutral + props.bad;
+  const average = total === 0 ? 0: (props.good - props.bad)/(total);
+  const positive = total === 0 ? 0: (props.good / total) * 100 + " %";
+
+  if (total === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
   return (
   <div>
     <table>
@@ -47,9 +34,9 @@ const Statistics = (props) => {
         <StatisticLine text="good" value={props.good} />
         <StatisticLine text="neutral" value={props.neutral} />
         <StatisticLine text="bad" value={props.bad} />
-        <StatisticLine text="all" value={props.total} />
-        <StatisticLine text="average" value={props.average} />
-        <StatisticLine text="positive" value={props.positive} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
       </tbody>
     </table>
   </div>
@@ -61,35 +48,14 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const setToGood = newValue => {
-    console.log('good value now', newValue)
-    setGood(newValue)
-  }
-
-  const setToNeutral = newValue => {
-    console.log('neutral value now', newValue)
-    setNeutral(newValue)
-  }
-
-  const setToBad = newValue => {
-    console.log('bad value now', newValue)
-    setBad(newValue)
-  }
-
-  let total = good + neutral + bad;
-
-  let average = total === 0 ? 0: (good - bad)/(total);
-
-  let positive = total === 0 ? 0: (good / total) * 100;
-
   return (
     <div>
       <h1> Give feedback </h1>
-      <Button onClick={() => setToGood(good + 1)} text="good" />
-      <Button onClick={() => setToNeutral(neutral + 1)} text="neutral" />
-      <Button onClick={() => setToBad(bad + 1)} text="bad" />
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
       <h1> Statistics </h1>
-      <History good = {good} neutral = {neutral} bad = {bad} total = {total} average = {average} positive = {positive} />
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />
     </div>
   )
 }
